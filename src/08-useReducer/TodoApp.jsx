10e5707dd10689ce608bf42cc30d488e8e00fb06
mbernallaura <mbernallaura@gmail.com>
2023-03-14 18:51:37 -0500

    Crear los componentes que se van utilizar y mostrar los todolist en la pagina

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,4 +1,5 @@
 import { useReducer } from "react"
+import { TodoList } from "./TodoList"
 import { todoReducer } from "./todoReducer"
 
 const initialState =[
@@ -9,30 +10,36 @@ const initialState =[
     },
     {
         id: new Date().getTime() * 3, 
-        description: 'Recolectar la piedra del alma',
+        description: 'Recolectar la piedra del tiempo',
         done: false,
     }
 ]
 
 export const TodoApp = () => {
     const [ todos, dispatch ] = useReducer(todoReducer, initialState )
+
+    const handleNewTodo = (todo) =>{
+        console.log({todo});
+    }
+
     return (
         <>
             <h1>TodoApp: 10,  <span>Pendientes: 2</span></h1>
             <hr />
             <div className="row">
                 <div className="col-7">
-                    <ul className="list-group">
+                    {/* <ul className="list-group">
                         {
                             todos.map(todo =>(
                                 <li key={todo.id} className="list-group-item d-flex justify-content-between">
-                                    <span className="align-self-center">Item 1</span> 
-                                    <button className="btn btn-danger">Borrar</button>
+                                <span className="align-self-center">Item 1</span> 
+                                <button className="btn btn-danger">Borrar</button>
                                 </li>
-                            ))
-                        }
-                        
-                    </ul>
+                                ))
+                            }
+                            
+                        </ul> */}
+                        <TodoList todos = {todos} />
                 </div>
 
                 <div className="col-5">
