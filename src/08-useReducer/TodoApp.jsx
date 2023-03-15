5802726f4569e5a210ba06810526d2e6038ae147
mbernallaura <mbernallaura@gmail.com>
2023-03-15 16:41:52 -0500

    Volver el componente TodoApp mas legible

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,4 +1,5 @@
 import { useReducer } from "react"
+import { TodoAdd } from "./TodoAdd"
 import { TodoList } from "./TodoList"
 import { todoReducer } from "./todoReducer"
 
@@ -28,37 +29,13 @@ export const TodoApp = () => {
             <hr />
             <div className="row">
                 <div className="col-7">
-                    {/* <ul className="list-group">
-                        {
-                            todos.map(todo =>(
-                                <li key={todo.id} className="list-group-item d-flex justify-content-between">
-                                <span className="align-self-center">Item 1</span> 
-                                <button className="btn btn-danger">Borrar</button>
-                                </li>
-                                ))
-                            }
-                            
-                        </ul> */}
-                        <TodoList todos = {todos} />
+                        <TodoList todos = { todos } />
                 </div>
 
                 <div className="col-5">
                     <h4>Agregar Todo</h4>
                     <hr />
-                    <form action="">
-                        <input 
-                            type="text" 
-                            placeholder="Que hay que hacer?"
-                            className="form-control"
-                        />
-
-                        <button
-                            type="submit"
-                            className="btn btn-outline-primary mt-1"
-                        >
-                            Agregar
-                        </button>
-                    </form>
+                    <TodoAdd onNewTodo={(todo)=> handleNewTodo(todo) }/>
                 </div>
             </div>
         </>
